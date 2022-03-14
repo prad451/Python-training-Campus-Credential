0cabf8a6f0f1b6c823365c7f862181c77205db42
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-03-14 20:41:17 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,17 @@
+#Datatypes in python
+
+#int datatype
+a = 15 
+print(type(a))
+
+#float datatype
+b = 12.30
+print(type(b))
+
+#boolean datatype
+c = True
+print(type(c))
+
+#string datatype
+d = "poonam"
+print(type(d))
