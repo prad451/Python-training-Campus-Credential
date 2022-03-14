38c069db91dfb3c5ff8d8e5d2076d5b2bad26eb4
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-03-14 20:38:22 +0530

    Add files via upload

---
+++ Python basics.py
@@ -0,0 +1,15 @@
+#variables
+
+#assigning value to a variables
+a = 12
+b = 20
+
+#changing assigned value
+b = "pragya"
+print(a)
+print(b)
+
+#assigning multiple values to a variables
+p,q,r = "poonam",45,0.20
+
+print(p,q,r)
