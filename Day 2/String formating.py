25aa7f7456e4a1e74150ac5b8efd6ad35aff2b2f
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-04-02 15:50:23 +0530

    Add files via upload

---
+++ Day 2/String formating.py
@@ -0,0 +1,27 @@
+#string formatting
+#method 1
+a = 'Poonam'
+b = 'Goa'
+c = 'Pune'
+ 
+print('I am {} and i want to go {}'.format(a,b))
+
+#method 2
+print('I am {0},i am from {1},i want to go {0}'.format(b,a))
+
+print('I am {0},i am from {2},i want to go {1}'.format(a,c,b))
+
+print('I am {0},i am from {1},i want to go {2}'.format(a,b,c))
+
+#method 3
+x = 10
+y = 5
+
+print(f"10*5 = {x * y}")
+
+print(f"I am {a},i am from {b},i want to go {'Satara'}")
+
+
+
+
+
