748169766bebe705ac0488eaa3aa2382cfe7ee08
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-06-02 23:55:00 +0530

    Update variables.py

--- Day 1/Variables/variables.py
+++ Day 1/Variables/variables.py
@@ -1,4 +1,16 @@
 #variables
+''' Variables are containers to store data '''
+
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)'''
+
+# Can start with: a-z, A - z , _
+# Cannot start with : 0-9
+# Can Contain : A-Z, a-z, 0-9, _
+# Cannot Contain : @, $ , % ,& , ^
 
 #assigning value to a variables
 a = 12
