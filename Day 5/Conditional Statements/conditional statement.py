bb8a0749c68820592233d3a90bbe9cc334a9896b
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-06-05 17:57:01 +0530

    Add files via upload

--- Day 5/Conditional Statements/conditional statement.py
+++ Day 5/Conditional Statements/conditional statement.py
@@ -1,1 +1,47 @@
+####conditional statements#####
+##1.if-else
+
+'''
+if condition:
+   statement
+else:
+   statement
+'''
+
+a=input("Enter first number : ")
+print(a)
+b=input("Enter second number : ")
+print(b)
+
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+    
+##2.elif:else-if
+
+'''
+if condition:
+    statement
+elif:
+    statement
+else:
+    statement
+'''
+
+marks=100
+
+if marks==100:
+    print("Student got out of marks!!")
+elif marks>75:
+    print("Student passed with distinction!!")
+elif marks>65:
+    print("Student passes with first class!!")
+elif marks>55:
+    print("Student passed with second class!!")
+elif marks>35:
+    print("Student passed with third class!!")
+else:
+    print("Student is failed!!")
+    
 
